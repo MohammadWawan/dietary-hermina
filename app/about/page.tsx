e04a6ece58127884cc431f514ed62d1ef2ecfef0
MohammadWawan <mohammadwawan.24@gmail.com>
2024-11-18 15:34:25 +0700

    revisi error deploy again

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import NavBar from "../components/navbar";
-
+import Image from "next/image";
 export default function about() {
   return (
     <div>
@@ -40,10 +40,12 @@ export default function about() {
             </h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
               <div className="bg-gray-100 rounded-lg p-4 shadow-md ">
-                <img
+                <Image
                   src="/wawan.jpg"
                   alt="Tim A"
                   className="w-full h-48 lg:h-auto object-cover rounded-t-lg"
+                  width={300}
+                  height={300}
                 />
                 <h3 className="text-xl font-semibold text-gray-800 mt-2 ">
                   Mohammad Wawan
